dea76b55a32240f86303341a03c6d1b37e58b135
JUNO-HOME <hacke11233@gmail.com>
2025-11-30 01:52:30 +0900

    cicd 5차 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Jenkins에서 VITE_BASE_PATH 를 넣어줄 예정 (예: "/qa/" 또는 "/")
+const basePath = process.env.VITE_BASE_PATH || '/'
+
 export default defineConfig({
   plugins: [react()],
+  base: basePath,   // ← 여기만 환경에 따라 달라짐
 })
